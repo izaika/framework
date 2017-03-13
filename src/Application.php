c6d29dd3586f4622d4230e6293f53ff1e639a979
Igor Zaika <izaika89@gmail.com>
2017-03-13 10:13:38 +0200

    Application class init

---
+++ src/Application.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Izaika\Framework;
+
+/**
+ * Class Application
+ * Singleton
+ *
+ * @package Izaika\Framework
+ */
+class Application
+{
+	private static $_instance = null;
+
+
+	public $config = [];
+
+
+	static public function getInstance(array $config = [])
+	{
+		if (is_null(self::$_instance)) {
+			self::$_instance = new self($config);
+		}
+		return self::$_instance;
+	}
+
+
+	private function __construct(array $config = [])
+	{
+		$this->config = $config;
+	}
+
+
+	public function run()
+	{
+		// TODO: implement run method
+	}
+
+
+	public function __destruct()
+	{
+		// TODO: implement __destruct method
+	}
+
+
+	private function __clone()
+	{
+
+	}
+}
