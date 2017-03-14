909d4376a26dadd3f845c9437661ace40167320c
Igor Zaika <izaika89@gmail.com>
2017-03-14 09:51:53 +0200

    Router (in progress)

---
+++ src/Router/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Izaika\Framework\Router;
+
+/**
+ * Class Router
+ * @package Izaika\Framework
+ */
+
+class Router
+{
+	public static $default_controller	= 'home';
+	public static $default_action		= 'index';
+
+
+}
